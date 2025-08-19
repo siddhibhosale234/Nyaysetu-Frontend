d17a0c962c5491186c5ed04d2332c15f60f59577
siddhibhosale234 <siddhibhosale@gmail.com>
2025-08-19 20:02:45 +0530

    viteconfig

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173, // optional, default is 5173
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // your backend server
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
